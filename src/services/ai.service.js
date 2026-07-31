386ec16f68a228b9d32c2f2f280f23047835bf26
Ehsanul Nabil <nabilehsanul@gmail.com>
2026-08-01 05:02:30 +0600

    T

--- src/services/ai.service.js
+++ src/services/ai.service.js
@@ -236,7 +236,7 @@ async function generatePdfFromHtml(htmlContent) {
 const browser = await puppeteer.launch({
     args: chromium.args,
     defaultViewport: chromium.defaultViewport,
-    executablePath: await chromium.executablePath(),
+    executablePath: await chromium.executablePath,
     headless: chromium.headless,
 });
 
